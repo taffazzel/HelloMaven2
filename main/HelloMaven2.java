76f4b5d15b419f407a01d2948cd8174933a54b02
Taffazzel Hossain <tafaz_hossain@yahoo.com>
2017-08-08 16:06:31 +0100

    HelloMaven added

---
+++ main/HelloMaven2.java
@@ -0,0 +1,11 @@
+public class HelloMaven2 {
+
+    private String name;
+    public HelloMaven2(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ test/HelloMaven2Test.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by tafaz on 8/8/2017.
+ */
+public class HelloMaven2Test {
+    @org.junit.Test
+    public void getName1() throws Exception {
+        HelloMaven2 hm = new HelloMaven("HelloMaven");
+	HelloMaven2 hm2 = new HelloMaven("Hello");
+        assertEquals("HelloMaven", hm.getName());
+	assertEquals("Hello", hm2.getName());
+
+    }
+
+}
