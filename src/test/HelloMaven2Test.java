5b9666f108ef6fda78536b64f9522ac26f32d991
Taffazzel Hossain <tafaz_hossain@yahoo.com>
2017-08-08 16:19:55 +0100

    Added

--- src/test/HelloMaven2Test.java
+++ src/test/HelloMaven2Test.java
@@ -6,8 +6,8 @@ import static org.junit.Assert.*;
 public class HelloMaven2Test {
     @org.junit.Test
     public void getName1() throws Exception {
-        HelloMaven2 hm = new HelloMaven("HelloMaven");
-	HelloMaven2 hm2 = new HelloMaven("Hello");
+        HelloMaven2 hm = new HelloMaven("HelloMave");
+	HelloMaven2 hm2 = new HelloMaven("Hell");
         assertEquals("HelloMaven", hm.getName());
 	assertEquals("Hello", hm2.getName());
 
